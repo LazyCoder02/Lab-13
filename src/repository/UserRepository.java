2fddc5fcd2c4ed8be8c91f9022d85a6651a1e668
Rafael <therafaeljr03@gmail.com>
2025-01-25 22:07:58 +0100

    everything

---
+++ src/repository/UserRepository.java
@@ -0,0 +1,5 @@
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
---
+++ src/service/UserService.java
@@ -0,0 +1,19 @@
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public boolean authenticateUser(String username, String password) {
+        return userRepository.findByUsername(username)
+                .map(user -> passwordEncoder.matches(password, user.getPassword()))
+                .orElse(false);
+    }
+}
